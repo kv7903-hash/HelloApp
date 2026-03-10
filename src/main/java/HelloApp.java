f5fdecaa9b484e10f7640f0bd8d391429e95bb3a
Khushi Vasa <kv7903@srmist.edu.in>
2026-03-10 09:11:43 +0530

    UC1: Display Hello World in console

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,13 @@
+/**
+ * UC1 - Display Hello World
+ * HelloApp prints a basic greeting message in the console
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
